e31a0dd603eccecbc32250af12b09c7f1a67e4c5
linbo <317293098@qq.com>
2020-11-05 09:07:44 +0800

    参数管理补充字段

--- src/main/java/com/example/demo/vo/ParamInfoVO.java
+++ src/main/java/com/example/demo/vo/ParamInfoVO.java
@@ -5,8 +5,12 @@ import lombok.Data;
 @Data
 public class ParamInfoVO {
 
-    private String paramName;
+    private String paramName;//参数名
 
-    private String paramValue;
+    private String paramValue;//参数值
+
+    private String paramMemo;//参数描述
+
+    private int paramInuse;//是否启用1：启用0：禁用
 
 }
