19685ad40f73231d953790b3dec95dde2883af2c
林博 <linb@epsoft.com.cn>
2026-04-01 09:57:53 +0800

    mybatis @Select@Update注解 用法

--- src/main/java/com/example/demo/mapper/DataMapper.java
+++ src/main/java/com/example/demo/mapper/DataMapper.java
@@ -2,9 +2,13 @@ package com.example.demo.mapper;
 
 import com.example.demo.entity.ParseLog;
 import com.example.demo.vo.ParamInfoVO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -24,4 +28,16 @@ public interface DataMapper {
     List<ParamInfoVO> selectParamInfoPage(ParamInfoVO paramInfo, RowBounds rowBounds);
 
     void insertParseLog(ParseLog parseLog);
+
+    @Select("select 1 name ,2 card from dual where akb999 = #{akb999}")
+    List<HashMap> selectBysql(@Param("akb999") String akb999);
+
+    @Update("update TALENT_POOL set source='第三方接口添加校验通过' where source='第三方接口添加' ")
+    void updateBysql(String sql);
+
+    /**
+     * 获取下一个序列号
+     * @return
+     */
+    Long getSequence();
 }
